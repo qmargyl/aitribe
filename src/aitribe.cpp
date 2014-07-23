cf10f54ead2bd9691457efa96f35eb0a7515206b
Martin Gyllensten <martin.gyllensten@gmail.com>
2014-07-23 17:29:43 +0200

    stepped up to mgframework 1.0.42.2

--- src/aitribe.cpp
+++ src/aitribe.cpp
@@ -23,20 +23,20 @@ bool AITribe::init(int w, int h, int tw, int th)
 		}
 
 		// All graphics should be loaded here.
-		m_Floor = loadBMPImage("tileset.bmp");
+		m_Floor = m_Window.loadBMPImage("tileset.bmp");
 
-		m_MOSprite[0] = loadBMPImage("movingobject_00.bmp");
-		m_MOSprite[1] = loadBMPImage("movingobject_01.bmp");
-		m_MOSprite[2] = loadBMPImage("movingobject_02.bmp");
-		m_MOSprite[3] = loadBMPImage("movingobject_03.bmp");
-		m_MOSprite[4] = loadBMPImage("movingobject_04.bmp");
-		m_MOSprite[5] = loadBMPImage("movingobject_05.bmp");
-		m_MOSprite[6] = loadBMPImage("movingobject_06.bmp");
-		m_MOSprite[7] = loadBMPImage("movingobject_07.bmp");
-		m_MOSprite[8] = loadBMPImage("movingobject_08.bmp");
+		m_MOSprite[0] = m_Window.loadBMPImage("movingobject_00.bmp");
+		m_MOSprite[1] = m_Window.loadBMPImage("movingobject_01.bmp");
+		m_MOSprite[2] = m_Window.loadBMPImage("movingobject_02.bmp");
+		m_MOSprite[3] = m_Window.loadBMPImage("movingobject_03.bmp");
+		m_MOSprite[4] = m_Window.loadBMPImage("movingobject_04.bmp");
+		m_MOSprite[5] = m_Window.loadBMPImage("movingobject_05.bmp");
+		m_MOSprite[6] = m_Window.loadBMPImage("movingobject_06.bmp");
+		m_MOSprite[7] = m_Window.loadBMPImage("movingobject_07.bmp");
+		m_MOSprite[8] = m_Window.loadBMPImage("movingobject_08.bmp");
 
-		m_StationaryObject = loadBMPImage("stationaryobject.bmp");
-		m_Mark = loadBMPImage("mark.bmp");
+		m_StationaryObject = m_Window.loadBMPImage("stationaryobject.bmp");
+		m_Mark = m_Window.loadBMPImage("mark.bmp");
 
 		SDL_SetColorKey(m_MOSprite[0], SDL_SRCCOLORKEY, 0);
 		SDL_SetColorKey(m_MOSprite[1], SDL_SRCCOLORKEY, 0);
@@ -61,16 +61,18 @@ bool AITribe::init(int w, int h, int tw, int th)
 		m_Map.setRightEdge(256);
 
 		// Setup application specific game logics..
-		activateSelectiveTileRendering();
-		setClientPlayer(MGF_PLAYER1);
-		activateOnlySelectOwnedMO();		// User can only select his own MO (with mouse click)
+		disableFeatureSelectiveTileRendering();
+		setClientPlayer(MGF_PLAYER1);			// Decides what index MO:s will be possible to select
+		enableFeatureOnlySelectOwnedMO();		// User can only select his own MO (with mouse click)
+		disableFeatureMouseScrolling();
+		enableFeatureCenterOnMO(0);				// A non-selectable MO
 
 		runConsoleCommand("setfps 60", this, NULL);			// Set initial FPS to 60 to avoid slow start
 		runConsoleCommand("logging off", this, NULL);		// Turn on logging for the MGFramework class
 		runConsoleCommand("map logging off", this, NULL);	// Turn on logging for the MGMap class
 		runConsoleCommand("minimap on", this, NULL);
-		runConsoleCommand("add so 2000", this, NULL);
-
+		
+		runConsoleCommand("add mo 1 -owner 0 -x 10 -y 10", this, NULL);
 		runConsoleCommand("add mo 20 -owner 1 -area_square 5 5 20 20", this, NULL);
 		runConsoleCommand("add mo 20 -owner 2", this, NULL);
 		runConsoleCommand("add mo 20 -owner 3", this, NULL);
@@ -79,6 +81,8 @@ bool AITribe::init(int w, int h, int tw, int th)
 		runConsoleCommand("add mo 20 -owner 6", this, NULL);
 		runConsoleCommand("add mo 20 -owner 7", this, NULL);
 		runConsoleCommand("add mo 20 -owner 8", this, NULL);
+
+		runConsoleCommand("add so 2000", this, NULL);
 
 		return true;
 	}
@@ -91,6 +95,11 @@ bool AITribe::init(int w, int h, int tw, int th)
 void AITribe::handleGameLogics()
 {
 	// Application specific game logics are updated here..
+	if(getNumberOfMO()>0)
+	{
+		m_Map.setScrollOffset(	m_Window.getWidth()/2 - m_MO[0].getTileX() * m_Map.getTileWidth() - m_MO[0].getXOffset(),
+								m_Window.getHeight()/2 - m_MO[0].getTileY() * m_Map.getTileHeight() - m_MO[0].getYOffset() );
+	}
 }
 
 void AITribe::draw()
@@ -113,7 +122,7 @@ void AITribe::draw()
 				{
 					if(m_Map.getTileProperty(x, y) & MGMAP_TP_PROPERTY_1)
 					{
-						drawSprite(m_Floor, getSurface(), 0, 0, x * m_Map.getTileWidth() + m_Map.getScrollX(), y * m_Map.getTileHeight() + m_Map.getScrollY(), m_Map.getTileWidth(), m_Map.getTileHeight());
+						m_Window.drawSprite(m_Floor, 0, 0, x * m_Map.getTileWidth() + m_Map.getScrollX(), y * m_Map.getTileHeight() + m_Map.getScrollY(), m_Map.getTileWidth(), m_Map.getTileHeight());
 					}
 					m_Map.unmarkForRendering(x, y);
 				}
@@ -126,14 +135,14 @@ void AITribe::draw()
 		{
 			if(m_MO != NULL)
 			{
-				oX=m_MO[i].getTileX() * m_Map.getTileWidth() + m_Map.getScrollX()+m_MO[i].getXOffset();
+				oX=m_MO[i].getTileX() * m_Map.getTileWidth() + m_Map.getScrollX() + m_MO[i].getXOffset();
 				oY=m_MO[i].getTileY() * m_Map.getTileHeight() + m_Map.getScrollY() + m_MO[i].getYOffset();
 				// Only draw visible moving objects...
-				if(detectCollisionRectangle(oX, oY, oX+m_Map.getTileWidth(), oY+m_Map.getTileHeight(), 0, 0, m_Window.getWidth() - m_Map.getRightEdge(), m_Window.getHeight() - m_Map.getBottomEdge()))
+				if(detectCollisionRectangle(oX, oY, oX + m_Map.getTileWidth(), oY + m_Map.getTileHeight(), 0, 0, m_Window.getWidth() - m_Map.getRightEdge(), m_Window.getHeight() - m_Map.getBottomEdge()))
 				{
 					if(m_MO[i].getOwner() >= 0 && m_MO[i].getOwner() < 9)
 					{
-						drawSprite(m_MOSprite[m_MO[i].getOwner()], getSurface(), 0, 0, oX, oY, m_Map.getTileWidth(), m_Map.getTileHeight());
+						m_Window.drawSprite(m_MOSprite[m_MO[i].getOwner()], 0, 0, oX, oY, m_Map.getTileWidth(), m_Map.getTileHeight());
 					}
 					if(isSelectiveTileRenderingActive())
 					{
@@ -150,7 +159,7 @@ void AITribe::draw()
 
 					if(m_MO[i].isMarked())
 					{
-						drawSprite(m_Mark, getSurface(), 0, 0, oX, oY, m_Map.getTileWidth(), m_Map.getTileHeight());
+						m_Window.drawSprite(m_Mark, 0, 0, oX, oY, m_Map.getTileWidth(), m_Map.getTileHeight());
 					}
 				}
 			}
@@ -167,20 +176,20 @@ void AITribe::draw()
 				// Only draw visible stationary objects...
 				if(detectCollisionRectangle(sX, sY, sX+m_Map.getTileWidth(), sY+m_Map.getTileHeight(), 0, 0, m_Window.getWidth() - m_Map.getRightEdge(), m_Window.getHeight() - m_Map.getBottomEdge()))
 				{
-					drawSprite(m_StationaryObject, getSurface(), 0, 0, sX, sY, m_Map.getTileWidth(), m_Map.getTileHeight()+16);
+					m_Window.drawSprite(m_StationaryObject, 0, 0, sX, sY, m_Map.getTileWidth(), m_Map.getTileHeight()+16);
 				}
 			}
 		}
 
 		// Draw a frame around the edge of the map
-		vLine32(getSurface(), m_Map.getLeftEdge(), m_Map.getTopEdge(), m_Map.getWindowHeight()-m_Map.getBottomEdge()-m_Map.getTopEdge(), 0x000000FF);
-		vLine32(getSurface(), m_Map.getWindowWidth()-m_Map.getRightEdge(), m_Map.getTopEdge(), m_Map.getWindowHeight()-m_Map.getBottomEdge()-m_Map.getTopEdge(), 0x000000FF);
-		hLine32(getSurface(), m_Map.getLeftEdge(), m_Map.getTopEdge(), m_Map.getWindowWidth()-m_Map.getLeftEdge()-m_Map.getRightEdge(), 0x000000FF);
-		hLine32(getSurface(), m_Map.getLeftEdge(), m_Map.getWindowHeight()-m_Map.getBottomEdge(), m_Map.getWindowWidth()-m_Map.getLeftEdge()-m_Map.getRightEdge(), 0x000000FF);
+		m_Window.vLine32(m_Map.getLeftEdge(), m_Map.getTopEdge(), m_Map.getWindowHeight()-m_Map.getBottomEdge()-m_Map.getTopEdge(), 0x000000FF);
+		m_Window.vLine32(m_Map.getWindowWidth()-m_Map.getRightEdge(), m_Map.getTopEdge(), m_Map.getWindowHeight()-m_Map.getBottomEdge()-m_Map.getTopEdge(), 0x000000FF);
+		m_Window.hLine32(m_Map.getLeftEdge(), m_Map.getTopEdge(), m_Map.getWindowWidth()-m_Map.getLeftEdge()-m_Map.getRightEdge(), 0x000000FF);
+		m_Window.hLine32(m_Map.getLeftEdge(), m_Map.getWindowHeight()-m_Map.getBottomEdge(), m_Map.getWindowWidth()-m_Map.getLeftEdge()-m_Map.getRightEdge(), 0x000000FF);
 
 
 		// Draw the mini map if enabled. Also draw all objects on it...
-		if(miniMapEnabled())
+		if(featureMiniMapEnabled())
 		{
 			for (int x=0; x < m_Map.getWidth(); x++)
 			{
@@ -195,38 +204,38 @@ void AITribe::draw()
 						// Different color for different tile property of each tile...
 						if(m_Map.getTileProperty(x, y)  & MGMAP_TP_PROPERTY_1)
 						{
-							putPixel32(getSurface(), x + m_Window.getWidth() - m_Map.getWidth() - 16, y + 16, 0x3F3F3F3F);
+							m_Window.putPixel32(x + m_Window.getWidth() - m_Map.getWidth() - 16, y + 16, 0x3F3F3F3F);
 						}
 						else if(m_Map.getTileProperty(x, y)  & MGMAP_TP_PROPERTY_2)
 						{
-							putPixel32(getSurface(), x + m_Window.getWidth() - m_Map.getWidth() - 16, y + 16, 0xFFFFFFFF);
+							m_Window.putPixel32(x + m_Window.getWidth() - m_Map.getWidth() - 16, y + 16, 0xFFFFFFFF);
 						}
 					}
 					else
 					{
 						// Draw it black..
-						putPixel32(getSurface(), x + m_Window.getWidth() - m_Map.getWidth() - 16, y + 16, 0x00000000);
+						m_Window.putPixel32(x + m_Window.getWidth() - m_Map.getWidth() - 16, y + 16, 0x00000000);
 					}
 				}
 			}
 			// Draw all moving objects on the mini map..
 			for(int i=0;i<getNumberOfMO();i++)
 			{
-				putPixel32(getSurface(), m_MO[i].getTileX() + m_Window.getWidth() - m_Map.getWidth() - 16, m_MO[i].getTileY() + 16, 0x00FF0000);
+				m_Window.putPixel32(m_MO[i].getTileX() + m_Window.getWidth() - m_Map.getWidth() - 16, m_MO[i].getTileY() + 16, 0x00FF0000);
 			}
 		}
 	}
 
 	// Example of how text can be printed on the surface.. Here FPS and time left between frames.
 #ifndef MGF_DEBUGGING_ENABLED
-	drawText(getSurface(), (string("MOs: ") + MGFramework::toString((int)getNumberOfMO()) + 
+	m_Window.drawText((string("MOs: ") + MGFramework::toString((int)getNumberOfMO()) + 
 			 string("(") + MGFramework::toString((int)MGMovingObject::nMovingMO()) + string(")") + string("          ")).c_str(), 
 			 16, m_Window.getWidth() - m_Map.getWidth() - 16, m_Map.getHeight() + 30, 0, 0, 0, 0, 255, 0);
-	drawText(getSurface(), (string("FD : ") + MGFramework::toString((int)getLastFrameDelayTime()) + string("          ")).c_str(), 
+	m_Window.drawText((string("FD : ") + MGFramework::toString((int)getLastFrameDelayTime()) + string("          ")).c_str(), 
 			 16, m_Window.getWidth() - m_Map.getWidth() - 16, m_Map.getHeight() + 50, 0, 0, 0, 0, 255, 0);
-	drawText(getSurface(), (string("FPS: ") + MGFramework::toString((int)getFPS()) + string("          ")).c_str(), 
+	m_Window.drawText((string("FPS: ") + MGFramework::toString((int)getFPS()) + string("          ")).c_str(), 
 			 16, m_Window.getWidth() - m_Map.getWidth() - 16, m_Map.getHeight() + 70, 0, 0, 0, 0, 255, 0);
-	drawText(getSurface(), (string("DT: ") + MGFramework::toString(getDrawnTilesCounter()) + string("          ")).c_str(), 
+	m_Window.drawText((string("DT: ") + MGFramework::toString(getDrawnTilesCounter()) + string("          ")).c_str(), 
 			 16, m_Window.getWidth() - m_Map.getWidth() - 16, m_Map.getHeight() + 90, 0, 0, 0, 0, 255, 0);
 #endif
 
@@ -240,10 +249,10 @@ void AITribe::draw()
 			int uLY=std::min(getFrameStartY(), getFrameEndY());
 			int xL=abs(getFrameStartX() - getFrameEndX());
 			int yL=abs(getFrameStartY() - getFrameEndY());
-			hLine32(getSurface(), uLX, uLY, xL, 0x00FF0000);
-			hLine32(getSurface(), uLX, uLY+yL, xL, 0x00FF0000);
-			vLine32(getSurface(), uLX, uLY, yL, 0x00FF0000);
-			vLine32(getSurface(), uLX+xL, uLY, yL, 0x00FF0000);
+			m_Window.hLine32(uLX, uLY, xL, 0x00FF0000);
+			m_Window.hLine32(uLX, uLY+yL, xL, 0x00FF0000);
+			m_Window.vLine32(uLX, uLY, yL, 0x00FF0000);
+			m_Window.vLine32(uLX+xL, uLY, yL, 0x00FF0000);
 		}
 	}
 
